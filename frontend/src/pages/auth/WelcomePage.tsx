0773d497b4cc4ff558cb08b630dfbf815d491a97
DavidRomero24 <davidhrq05@gmail.com>
2025-04-10 20:02:35 -0500

    modify text

--- frontend/src/pages/auth/WelcomePage.tsx
+++ frontend/src/pages/auth/WelcomePage.tsx
@@ -1,23 +1,4 @@
-// RegisterPage.tsx
 import Welcome from "../../components/auth/Welcome";
-// export default function WelcomePage() {
-//   return <Welcome />;
-// }
-
-
-
-// import Welcome from '../../components/auth/Welcome';
-// export default function WelcomePage(){
-// const WelcomePage = () => {
-//   return (
-//     <div className="flex justify-center items-center h-screen bg-gray-100">
-//       <Welcome />
-//     </div>
-//   );
-// };
-
-// export default WelcomePage();
-// }
 
 const WelcomePage = () => {
     return (
